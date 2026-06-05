ba0b42ba99c24c814f1758178142bcac130aa5c4
hugojarkoff <hugo.jarkoff@gmail.com>
2026-06-05 15:21:02 +0200

    loop client connection messages

--- client.cpp
+++ client.cpp
@@ -11,23 +11,25 @@ int main() {
 	
 	// specifying address
 	sockaddr_in serverAddress;
-	serverAddress.sin_family = AF_INET;
-	serverAddress.sin_port = htons(8080);
-	serverAddress.sin_addr.s_addr = INADDR_ANY;
+	serverAddress.sin_family = AF_INET;  // Address family, AF_INET = IPv4
+	serverAddress.sin_port = htons(8080);  // Port, with endian conversion -> Host TO Network Small
+	serverAddress.sin_addr.s_addr = INADDR_ANY;  // Internet address
 
 	// sending connection request to connect to server 
 	if ( connect(
 		clientSocket, 
-		reinterpret_cast<sockaddr*> (&serverAddress), 
+		reinterpret_cast<sockaddr*> (&serverAddress), // connect wants a sockaddr, but we use an internet one - sockaddr_in
 		sizeof(serverAddress)
 	) == -1 ) { return -1; }; // ERROR
 								
-	// sending data
-	std::string message;	
-	std::cout << "Enter your message: ";
-	std::getline(std::cin, message); 
+	while (true) {
+		// sending data
+		std::string message;	
+		std::cout << "Enter your message: ";
+		std::getline(std::cin, message); 
 
-	send(clientSocket, message.c_str(), message.length(), 0);
+		send(clientSocket, message.c_str(), message.length(), 0);
+	}
 	
 	// close socket
 	close(clientSocket);
