2d36545c3aff07ca72f966e8d309fae7034aaf25
hugojarkoff <hugo.jarkoff@gmail.com>
2026-06-05 17:27:32 +0200

    Remove useless comments, client code isnt complicated

--- client.cpp
+++ client.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
-#include <netinet/in.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <unistd.h>
-#include <string.h>
 
 int main() {
 
 	// creating socket
 	int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 	
-	// specifying address
+	// specifying local server address
 	sockaddr_in serverAddress;
-	serverAddress.sin_family = AF_INET;  // Address family, AF_INET = IPv4
-	serverAddress.sin_port = htons(8080);  // Port, with endian conversion -> Host TO Network Small
-	serverAddress.sin_addr.s_addr = INADDR_ANY;  // Internet address
+	serverAddress.sin_family = AF_INET;	
+	serverAddress.sin_port = htons(8080);
+	serverAddress.sin_addr.s_addr = INADDR_ANY;
 
 	// sending connection request to connect to server 
 	if ( connect(
 		clientSocket, 
-		reinterpret_cast<sockaddr*> (&serverAddress), // connect wants a sockaddr, but we use an internet one - sockaddr_in
+		reinterpret_cast<sockaddr*> (&serverAddress),
 		sizeof(serverAddress)
-	) == -1 ) { return -1; }; // ERROR
+	) == -1 ) { return -1; }; 
 								
 	while (true) {
-		// sending data
+
 		std::string message;	
 		std::cout << "Enter your message: ";
 		std::getline(std::cin, message); 
 
 		send(clientSocket, message.c_str(), message.length(), 0);
+
 	}
 	
 	// close socket
